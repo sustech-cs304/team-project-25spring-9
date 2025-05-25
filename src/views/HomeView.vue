c91fe9a198a6db054521b1df867f8a60231ea3d7
IT-Bill <704317542@qq.com>
2025-05-25 11:06:09 +0000

    Update dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,8 @@ import {
   mdiChartTimelineVariant,
   mdiImageMultiple,
   mdiArchiveClockOutline,
+  mdiAccount,
+  mdiTag
 } from '@mdi/js'
 import * as chartConfig from '@/components/Charts/chart.config.js'
 import SectionMain from '@/components/SectionMain.vue'
@@ -13,13 +15,38 @@ import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
 
 const chartData = ref(null)
+const photoCount = ref(0)
+const peopleCount = ref(0)
+const tagCount = ref(0)
 
 const fillChartData = () => {
   chartData.value = chartConfig.sampleChartData()
 }
 
+const fetchCounts = async () => {
+  try {
+    // Fetch photo count
+    const photoResponse = await fetch('http://10.16.60.67:9090/img/getimgcnt?userId=' + mainStore.userId)
+    const photoData = await photoResponse.json()
+    photoCount.value = photoData.data || 0
+
+    // Fetch people count
+    const peopleResponse = await fetch('http://10.16.60.67:9090/img/getpeoplecnt?userId=' + mainStore.userId)
+    const peopleData = await peopleResponse.json()
+    peopleCount.value = peopleData.data || 0
+
+    // Fetch tag count
+    const tagResponse = await fetch('http://10.16.60.67:9090/img/gettagcnt?userId=' + mainStore.userId)
+    const tagData = await tagResponse.json()
+    tagCount.value = tagData.data || 0
+  } catch (error) {
+    console.error('Error fetching counts:', error)
+  }
+}
+
 onMounted(() => {
   fillChartData()
+  fetchCounts()
 })
 
 const mainStore = useMainStore()
@@ -39,20 +66,20 @@ const transactionBarItems = computed(() => mainStore.history)
         <CardBoxWidget
           color="text-green-500"
           :icon="mdiImageMultiple"
-          :number="114514"
-          label="Total Photos"
+          :number="photoCount"
+          label="Photos"
         />
         <CardBoxWidget
           color="text-blue-500"
-          :icon="mdiArchiveClockOutline"
-          :number="365"
-          label="Days"
+          :icon="mdiAccount"
+          :number="peopleCount"
+          label="People"
         />
         <CardBoxWidget
-          color="text-blue-500"
-          :icon="mdiArchiveClockOutline"
-          :number="365"
-          label="Days"
+          color="text-purple-500"
+          :icon="mdiTag"
+          :number="tagCount"
+          label="Tags"
         />
       </div>
 
